6ad35e51ee26f6c1d04669751504682a7b595a66
Stephen Jefferson <stephen@sjefferson.co.uk>
2022-01-30 16:47:59 +0000

    Working 2 way I2c example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import machine
+import json
 
 sda1 = machine.Pin(2)
 scl1 = machine.Pin(3)
@@ -10,8 +11,12 @@ if devices:
     for i in devices:
         print(i)
 
+i2c_lights = 0x41
 
 print("command register config")
+
+#10000000 Get/set config data
+#10000001 Get group assignments
 
 #01GRIIII Set LED values
 #data byte LED duty cycle
@@ -19,7 +24,7 @@ print("command register config")
 group = True #(G)
 reset = True #(R)
 id = 0 # (IIII)
-duty = 0
+duty = 255
 
 commandRegister = 0b00000000
 
@@ -45,6 +50,42 @@ data.append(duty)
 while len(data) < 8:
     data.append(0)
 
-senddata = bytearray(data)
+sendData = bytearray(data)
 
-i2c1.writeto(0x41, senddata)
+if i2c_lights in devices:
+    i2c1.writeto(i2c_lights, sendData)
+
+    returnData = i2c1.readfrom(i2c_lights, 1)
+
+    print(returnData)
+
+    returnData = int(returnData[0])
+    print(returnData)
+
+    if returnData > 0:
+        if returnData & 0b00000010:
+            print ("Group config out of date, reloading")
+            data = []
+            data.append(0b10000001)
+            while len(data) < 8:
+                data.append(0)
+            sendData = bytearray(data)
+            i2c1.writeto(i2c_lights, sendData)
+
+            incomingBytes = i2c1.readfrom(i2c_lights, 2)
+            print(incomingBytes)
+            length = int.from_bytes(incomingBytes, "big")
+            print(length)
+            returnData = i2c1.readfrom(i2c_lights, length)
+            led_groups = {}
+            led_groups = json.loads(returnData)
+            print(led_groups)
+
+        else:
+            print("unknown error")
+
+    else:
+        print("Successful update")
+
+else:
+    print("No valid devices")
